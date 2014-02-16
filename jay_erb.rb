a89f5879fcd8223dbfcef3cce5f649558bebb251
emertx <anonyomous@emertx.com>
2014-02-15 23:56:15 -0500

    Wrong approach. commiting before pull which will delete the file

--- jay_erb.rb
+++ jay_erb.rb
@@ -4,25 +4,32 @@ module JERuby
 
   module_function
 
-  @erb_parser = /(?<pre_ruby>.*)<\*(?<ruby>.*)\*>(?<post_ruby>.*)/
+  @line_parser = /(?<pre_ruby>.*)<\*(?<ruby>.*)\*>(?<post_ruby>.*)/
   @ruby_tag = /<\*(?<ruby>.*)\*>/
+  @buffer = ''
+  @loop_count = 0
 
-  def json_to_hash
-    JSON.parse File.read @data_file
+  def json_to_hash(data_file)
+    JSON.parse File.read data_file
   end
 
   def generate_html(erb_file, data_file)
-    @data_file = data_file
-    @erb_file = erb_file
-    @template_data = json_to_hash
-    run_file_through_engine 
+    @template_data = json_to_hash data_file
+    run_file_through_engine erb_file
   end
 
-  def run_file_through_engine
+  def run_file_through_engine(erb_file)
     output = File.open 'jay_output', 'w'
-    file = File.read @erb_file
+    file = File.read erb_file
      file.each_line do |line|
-      out = contains_ruby?(line) ? run_line_through_engine(line) : line
+      case contains_ruby?(line)
+      when 'no'
+        out = line
+      when 'yes, normal'
+        out = run_line_through_engine(line, false)
+      when 'yes, loop'
+        out = run_line_through_engine(line, true)
+      end
       output.puts out
     end
     puts output
@@ -30,14 +37,27 @@ module JERuby
 
   def contains_ruby?(line)
     m = line.match @ruby_tag
-    m ? (return true) : (return false)
+    if m.nil?
+      return 'no'
+    else
+      if m[:ruby].include? 'EACH'
+        return 'yes, loop'
+      else
+        return 'yes, normal'
+      end
+    end
   end
 
-  def run_line_through_engine(line)
-    line_match = line.match @erb_parser
-    ruby_output = get_data line_match[:ruby]
-    "#{line_match[:pre_ruby]} #{ruby_output} #{line_match[:post_ruby]}" 
+  def run_line_through_engine(line, contains_loop)
+    add_to_loop_count if contains_loop
+    line_match = line.match @line_parser    
+      
+      ruby_output = get_data line_match[:ruby]
+      "#{line_match[:pre_ruby]} #{ruby_output} #{line_match[:post_ruby]}" 
+    end
   end
+
+  def 
 
   def get_data(ruby)
     keys = ruby.split"."
@@ -47,6 +67,15 @@ module JERuby
     end
     ans = keys.join('')
     eval "@template_data#{ans}"
+  end
+
+  def write_to_file?
+    return true if @loop_count == 0
+    return false
+  end
+
+  def add_to_loop_count
+    @loop_count = @loop_count + 1
   end
 
 end
